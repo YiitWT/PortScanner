1dc7aec9df069fac6c3589ec1cd750870ef77d21
YiitWT <99076111+YiitWT@users.noreply.github.com>
2023-04-15 14:10:07 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,25 @@
+
+# YIIT'S PORT SCANNER ⚓
+
+A basic tool for **only** port-scanning 
+
+
+## INSTALL & USAGE
+#### Clone from github
+
+```
+$ git clone https://github.com/yiitwt/portscanner
+```
+Next
+```
+$ cd portscanner
+```
+after that
+```
+$ python main.py
+```
+
+### DONT FORGET
+Don't delet the ports.txt
+## IMAGES
+![exampleimage](https://i.imgur.com/XEYYygm.png)
---
+++ main.py
@@ -0,0 +1,34 @@
+import pyfiglet
+import sys
+import socket
+from datetime import datetime
+
+ascii_banner = pyfiglet.figlet_format("YIIT'S SCANNER")
+print(ascii_banner)
+target = input(str("Target ip: "))
+
+
+print("-" * 50)
+print("Scanning target", target)
+print("Scanning started at: "+ str(datetime.now()))
+print("-" * 50)
+
+try:
+    for port in range(80, 90):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        socket.setdefaulttimeout(0.5)
+        #Açık portu göster
+        result = s.connect_ex((target, port))
+        if result == 0:
+            file = open("ports.txt", "w")
+            print("[*] Port {} is open".format(port))
+            file.write("[*] Port {} is open".format(port) + " ip: "+str(target)+"\n")
+            file.close()
+        s.close()
+except KeyboardInterrupt:
+    print("\n Kapatılıyor")
+    sys.exit()
+
+except socket.error:
+    print("Host cevap vermeyi bıraktı :'(")
+    sys.exit()
